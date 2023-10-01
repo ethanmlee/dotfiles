2bc59c117aaefa850b9d9963bab6440fe3de3f16
Ethan M. Lee <ethan@mlee.xyz>
2023-10-01 02:55:47 -0400

    nerdtree stuff

---
+++ .config/nvim/after/plugin/nerdtree.lua
@@ -0,0 +1,12 @@
+vim.g.NERDTreeGitStatusIndicatorMapCustom = {
+    Modified  = 'M',
+    Staged    = 'A',
+    Untracked = '?',
+    Renamed   = 'R',
+    Unmerged  = 'U',
+    Deleted   = 'D',
+    Dirty     = 'd',
+    Ignored   = '!',
+    Clean     = 'C',
+    Unknown   = '???'
+}
