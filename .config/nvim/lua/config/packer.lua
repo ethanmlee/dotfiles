7dbdad410670e03b1e2a4d37edc8b26ae5dc04fe
Ethan M. Lee <ethan@mlee.xyz>
2024-07-19 13:46:38 -0400

    fix treesitter

--- .config/nvim/lua/config/packer.lua
+++ .config/nvim/lua/config/packer.lua
@@ -5,47 +5,47 @@ vim.cmd [[packadd packer.nvim]]
 
 -- Define the post-installation function
 local post_installation = function()
-    -- Call the specific UpdateColors() function from `colors.lua`.
-    require("after.plugin.colors").UpdateColors()
+		-- Call the specific UpdateColors() function from `colors.lua`.
+		require("after.plugin.colors").UpdateColors()
 end
 
 return require('packer').startup(function(use)
 
 	-- PACKER --
-  -- https://github.com/wbthomason/packer.nvim
+	-- https://github.com/wbthomason/packer.nvim
 	use 'wbthomason/packer.nvim'
 
-  -- TELESCOPE --
-  -- https://github.com/nvim-telescope/telescope.nvim
+	-- TELESCOPE --
+	-- https://github.com/nvim-telescope/telescope.nvim
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.3',
-		-- or                            , branch = '0.1.x',
+		-- or,                           branch = '0.1.x',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
 
-  -- HARPOON --
-  -- https://github.com/ThePrimeagen/harpoon
-  use {
-    'ThePrimeagen/harpoon', 
+	-- HARPOON --
+	-- https://github.com/ThePrimeagen/harpoon
+	use {
+		'ThePrimeagen/harpoon', 
 		requires = { {'nvim-lua/plenary.nvim'} }
-  }
+	}
 
-  -- BASE16 --
-  -- https://github.com/chriskempson/base16-vim
-  use ('chriskempson/base16-vim')
+	-- BASE16 --
+	-- https://github.com/chriskempson/base16-vim
+	use ('chriskempson/base16-vim')
 
---  -- TREESITTER --
---  -- https://github.com/nvim-treesitter/nvim-treesitter
---	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
---	use('nvim-treesitter/playground')
+	 -- TREESITTER --
+	 -- https://github.com/nvim-treesitter/nvim-treesitter
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use('nvim-treesitter/playground')
 
-  -- UNDOTREE --
-  -- https://github.com/mbbill/undotree
+	-- UNDOTREE --
+	-- https://github.com/mbbill/undotree
 	use('mbbill/undotree')
 
 
-  -- LSP-ZERO --
-  -- https://github.com/VonHeikemen/lsp-zero.nvim
+	-- LSP-ZERO --
+	-- https://github.com/VonHeikemen/lsp-zero.nvim
 	use {
 		'VonHeikemen/lsp-zero.nvim',
 		branch = 'v3.x',
@@ -63,25 +63,25 @@ return require('packer').startup(function(use)
 		}
 	}
 
-  -- VIM-DEVICONS --
-  -- https://github.com/ryanoasis/vim-devicons
-  use('ryanoasis/vim-devicons')
+	-- VIM-DEVICONS --
+	-- https://github.com/ryanoasis/vim-devicons
+	use('ryanoasis/vim-devicons')
 
-  -- VIM-VISUAL-MULTI --
-  -- https://github.com/mg979/vim-visual-multi
-  use('mg979/vim-visual-multi')
+	-- VIM-VISUAL-MULTI --
+	-- https://github.com/mg979/vim-visual-multi
+	use('mg979/vim-visual-multi')
 
-  -- kinda sucks should find another one
-  -- NERDTREE --
-  -- https://github.com/preservim/nerdtree
-  use('preservim/nerdtree')
-  use('Xuyuanp/nerdtree-git-plugin')
+	-- kinda sucks should find another one
+	-- NERDTREE --
+	-- https://github.com/preservim/nerdtree
+	use('preservim/nerdtree')
+	use('Xuyuanp/nerdtree-git-plugin')
 
-  -- LUALINE --
-  -- https://github.com/nvim-lualine/lualine.nvim
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-  }
+	-- LUALINE --
+	-- https://github.com/nvim-lualine/lualine.nvim
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+	}
 
 end)
