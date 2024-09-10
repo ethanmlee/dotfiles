9a04b3306718f23e92bb7b0626716ddd0e5e0d83
Ethan M. Lee <ethan@mlee.xyz>
2024-09-09 20:56:10 -0400

    Comment plugin

--- .config/nvim/config/plug.lua
+++ .config/nvim/config/plug.lua
@@ -1,29 +1,35 @@
 local vim = vim
 local Plug = vim.fn['plug#']
 local plugin_configs = {}
+local function build_plugin_configs(name)
+    table.insert(plugin_configs, function() require("plugins." .. name) end)
+end
+local function setup_plugin(plugin_name)
+    table.insert(plugin_configs, function() require(plugin_name).setup {} end)
+end
 
 vim.call('plug#begin', '~/.local/share/nvim/plugged')
 
 -- AUTOPAIRS --
 -- https://github.com/windwp/nvim-autopairs
 Plug('windwp/nvim-autopairs')
-table.insert(plugin_configs, function() require('nvim-autopairs').setup {} end)
+setup_plugin("nvim-autopairs")
 
 -- TELESCOPE --
 -- https://github.com/nvim-telescope/telescope.nvim
 Plug('nvim-lua/plenary.nvim')
 Plug('nvim-telescope/telescope.nvim', { ['tag'] = '0.1.8' })
-table.insert(plugin_configs, function() require("plugins.telescope") end)
+build_plugin_configs("telescope")
 
 -- HARPOON --
 -- https://github.com/ThePrimeagen/harpoon
 Plug('ThePrimeagen/harpoon')
-table.insert(plugin_configs, function() require("plugins.harpoon") end)
+build_plugin_configs("harpoon")
 
 -- BASE16 --
 -- https://github.com/chriskempson/base16-vim
 Plug('chriskempson/base16-vim')
-table.insert(plugin_configs, function() require("plugins.colors") end)
+build_plugin_configs("colors")
 
 -- COLORIZER --
 -- https://github.com/norcalli/nvim-colorizer.lua
@@ -33,12 +39,12 @@ Plug('norcalli/nvim-colorizer.lua')
 -- https://github.com/nvim-treesitter/nvim-treesitter
 Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate'})
 Plug('nvim-treesitter/playground')
-table.insert(plugin_configs, function() require("plugins.treesitter") end)
+build_plugin_configs("treesitter")
 
 -- UNDOTREE --
 -- https://github.com/mbbill/undotree
 Plug('mbbill/undotree')
-table.insert(plugin_configs, function() require("plugins.undotree") end)
+build_plugin_configs("undotree")
 
 -- LSP-ZERO --
 -- https://github.com/VonHeikemen/lsp-zero.nvim
@@ -49,7 +55,7 @@ Plug('neovim/nvim-lspconfig')
 Plug('hrsh7th/nvim-cmp')
 Plug('hrsh7th/cmp-nvim-lsp')
 Plug('L3MON4D3/LuaSnip')
-table.insert(plugin_configs, function() require("plugins.lsp") end)
+build_plugin_configs("lsp")
 
 -- VIM-DEVICONS --
 -- https://github.com/ryanoasis/vim-devicons
@@ -58,24 +64,29 @@ Plug('ryanoasis/vim-devicons')
 -- VIM-VISUAL-MULTI --
 -- https://github.com/mg979/vim-visual-multi
 Plug('mg979/vim-visual-multi')
-table.insert(plugin_configs, function() require("plugins.visual-multi") end)
+build_plugin_configs("visual-multi")
 
 -- kinda sucks should find another one
 -- NERDTREE --
 -- https://github.com/preservim/nerdtree
 Plug('preservim/nerdtree')
 Plug('Xuyuanp/nerdtree-git-plugin')
-table.insert(plugin_configs, function() require("plugins.nerdtree") end)
+build_plugin_configs("nerdtree")
 
 -- LUALINE --
 -- https://github.com/nvim-lualine/lualine.nvim
 Plug('nvim-lualine/lualine.nvim')
 Plug('nvim-tree/nvim-web-devicons')
-table.insert(plugin_configs, function() require("plugins.lualine") end)
+build_plugin_configs("lualine")
 
 -- GIT BLAME --
 -- https://github.com/f-person/git-blame.nvim
 --Plug('f-person/git-blame.nvim')
+
+-- Commenting
+-- https://github.com/numToStr/Comment.nvim
+Plug("numToStr/Comment.nvim")
+setup_plugin("Comment")
 
 vim.call('plug#end')
 
